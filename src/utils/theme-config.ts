d72a2c80e42326302058973acb5674f00340add4
Andrii Koptiev <94103251+andrii-koptiev@users.noreply.github.com>
2023-10-10 17:48:37 +0300

    ui library added (#1)

---
+++ src/utils/theme-config.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@rneui/themed';
+
+export const theme = createTheme({
+  // Use only one color scheme
+  lightColors: {
+    primary: 'red',
+  },
+  // And set that mode as default
+  mode: 'light',
+  components: {},
+});
